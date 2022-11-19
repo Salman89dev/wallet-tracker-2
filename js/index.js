185c5bc8e5d4c84f875f4a3e6e513de781f08fce
Salman89dev <hmsalmansadiq@gmail.com>
2022-11-19 02:27:29 -0800

    done with auth

--- js/index.js
+++ js/index.js
@@ -1,1 +1,63 @@
-console.log("asdas")
+var auth=firebase.auth();
+var firestore=firebase.firestore();
+
+
+let signInFrom=document.querySelector(".signInFrom")
+let signUpFrom=document.querySelector(".signUpFrom")
+
+
+let signInFormSubmission=async(e)=>
+{
+    e.preventDefault();
+    try {
+
+        let email=document.querySelector(".signInEmail").value;
+        let password=document.querySelector(".signInPassword").value;
+        if(email && password)
+        {
+            let {user:{uid}}=await auth.signInWithEmailAndPassword(email,password)
+    
+            let userInfo=await firestore.collection("user").doc(uid).get();
+            console.log(userInfo.data())
+        }
+
+    } catch (error) {
+        console.log(error.message)
+    }
+
+}
+
+
+let signUpFormSubmission=async(e)=>
+{
+    e.preventDefault();
+    try {
+        let name=document.querySelector(".signUpName").value;
+        let email=document.querySelector(".signUpEmail").value;
+        let password=document.querySelector(".signUpPassword").value;
+        if(name && email && password)
+        {
+           let {user:{uid}}= await auth.createUserWithEmailAndPassword(email,password)
+            let userInfo={
+                displayName:name,
+                email:email,
+                createdAt:new Date()
+            }
+
+            await firestore.collection("user").doc(uid).set(userInfo);
+            console.log("done")
+
+        }
+    } catch (error) {
+        console.log(error.message)
+    }
+
+}
+
+signUpFrom.addEventListener("submit",(e)=>signUpFormSubmission(e))
+signInFrom.addEventListener("submit",(e)=>signInFormSubmission(e))
+
+
+
+
+
